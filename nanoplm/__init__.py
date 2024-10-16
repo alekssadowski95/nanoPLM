83f15d2234518d8683a2172fe20fe6257cf31475
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2024-10-16 15:29:01 +0200

    Update __init__.py

--- nanoplm/__init__.py
+++ nanoplm/__init__.py
@@ -8,6 +8,28 @@ from flask_sqlalchemy import SQLAlchemy
 # setup project paths
 MODULE_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
+# Create flask app instance
+application = app = Flask(__name__)
+
+# Add database
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# Add secret key
+app.config['SECRET_KEY'] = 'afs87fas7bfsa98fbasbas98fh78oizu'
+
+# Check user progres
+app.config['FIRST_STARTUP'] = True
+TUTORIAL_COMPLETE = False
+
+# nanoPLM modules
+app.config['FREECADCMD'] = False
+app.config['FREECADGUI'] = False
+app.config['PREPOMAX'] = False
+
+# FreeCAD imports
 # Add path to FreeCAD Python interface
 # path to your FreeCAD.so or FreeCAD.dll file
 FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD 0.21/bin'
@@ -16,6 +38,7 @@ sys.path.append(FREECAD_ABS_PATH)
 # import the FreeCAD Python interface
 try:
     import FreeCAD
+    app.config['FREECADCMD'] = True
     print('The FreeCAD Python interface has been loaded')
 except ValueError:
     print('FreeCAD library not found. Please check the FREECADPATH variable in the import script is correct')
@@ -36,24 +59,6 @@ try:
     import _PartDesign as PartDesign
 except:
     print('_PartDesign (Linux PPA) could not be imported (ignore if on Windows)')
-
-
-
-# Create flask app instance
-application = app = Flask(__name__)
-
-# Add database
-db = SQLAlchemy()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-# Add secret key
-app.config['SECRET_KEY'] = 'afs87fas7bfsa98fbasbas98fh78oizu'
-
-# Check user progres
-app.config['FIRST_STARTUP'] = True
-TUTORIAL_COMPLETE = False
 
 # Add routes to app
 from nanoplm import routes
