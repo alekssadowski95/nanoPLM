291c06952d49723ce0b1ad6b7178b28d712b74e8
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2024-10-17 21:34:00 +0200

    update

--- nanoplm/sample.py
+++ nanoplm/sample.py
@@ -4,6 +4,7 @@ products = [
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 1,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -18,13 +19,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "b1740fab54843dcc",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 2,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -39,13 +42,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "71e87e5cf701b274",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 3,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -60,13 +65,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "234c8bcc61648ab4",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 4,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -81,13 +88,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "56285557f945357c",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 5,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -102,13 +111,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "9bac44cbb245f7d2",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 6,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -123,13 +134,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "90010007b0ebfe74",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 7,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -144,13 +157,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "1813e1c698ccf8d1",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 8,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -165,13 +180,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "30f6c342d58f1a9f",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 9,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -186,13 +203,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "82bde1f70ded0030",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 10,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -207,13 +226,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "d29738dfa19f628e",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 11,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -228,13 +249,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "248473fa0bcb3aa5",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 12,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -249,13 +272,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "a241a2f37102fc81",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 13,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -270,13 +295,15 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     },
     {
         "uuid": "e1a24e2bb6364e30",
         "name": "Sägeblatt",
         "description": "Sägeblatt für Harthölzer",
         "type": "SBHH",
+        "number": 14,
         "stammblattbreite": 2.0,
         "plattensitzhoehe": 3.5,
         "plattensitzlaenge": 3.55,
@@ -291,6 +318,7 @@ products = [
         "preview_available": False,
         "techdraw_available": False,
         "mfg_available": False,
-        "outdated_data": False
+        "outdated_data": False,
+        "active": True
     }
 ]
