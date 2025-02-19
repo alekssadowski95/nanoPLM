6317a0b55d4c5944c9d14edd74614404cf1fc74d
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-02-19 10:09:05 +0100

    Update create_dist.bat

--- create_dist.bat
+++ create_dist.bat
@@ -1,1 +1,7 @@
-python -m PyInstaller run.py --add-data nanoplm:nanoplm
+@set mypath=%cd%
+
+cd %mypath%
+
+python -m PyInstaller run_local.py --icon "nanoplm/static/nanoplm-logo.png" --add-data="nanoplm/templates/*:nanoplm/templates" --add-data="nanoplm/static/*:nanoplm/static" --add-data="nanoplm/site.db:nanoplm/site.db" --add-data="LICENSE:." --splash nanoplm/static/nanoplm-splash.jpg --noconfirm
+
+@Pause
